d3462543423441cf18fa8b0cf89692e361c61699
Dmitriy Khomiakov <unstopddd@gmail.com>
2021-01-15 20:05:43 +0200

    Add React-hook-form & React-input-mask

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
+import InputMask from 'react-input-mask';
 import contactsActions from '../../redux/contacts/contacts-actions';
 import { getContacts } from '../../redux/contacts/contacts-selectors';
 
@@ -12,14 +14,18 @@ export default function ContactForm() {
   const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = data => {
+    const { name, number } = data;
+    // e.preventDefault();
 
     if (checkName(name)) {
       return alert(`${name} is already in contacts`);
     }
 
     dispatch(contactsActions.addContact(name, number));
+
     resetInputForm();
   };
 
@@ -36,24 +42,52 @@ export default function ContactForm() {
   };
 
   return (
-    <form className={s.form} onSubmit={handleSubmit}>
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
       <label className={s.label}>
         <input
+          ref={register({
+            required: true,
+            minLength: 3,
+            maxLength: 15,
+          })}
           className={s.name}
           type="text"
           name="name"
           value={name}
           onChange={e => setName(e.target.value)}
         />
+        {errors.name && (
+          <p className={s.errorText}>
+            Name must be at least 3 characters and maximum 15 characters
+          </p>
+        )}
       </label>
       <label className={s.label}>
-        <input
+        <InputMask
+          ref={register}
+          className={s.number}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+          mask="+3 8(099) 999-99-99"
+        />
+        {/* <input
+          ref={register({
+            required: true,
+            pattern: /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
+          })}
           className={s.number}
           type="tel"
           name="number"
           value={number}
           onChange={e => setNumber(e.target.value)}
         />
+        {errors.number && (
+          <p className={s.errorText}>
+            It must be a number between 10 and 14 characters
+          </p>
+        )} */}
       </label>
       <button className={s.button} type="submit">
         Add contact
